f9d5dff62ad94420f1bbb296e82eb1a5d87f0aab
Onur <gardbrom@gmail.com>
2021-12-28 18:30:20 +0300

    Initial commit

---
+++ Source/MEDELT/MEDELTPlayerController.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MEDELTPlayerController.h"
+
+AMEDELTPlayerController::AMEDELTPlayerController()
+{
+	bShowMouseCursor = true;
+	bEnableClickEvents = true;
+	bEnableTouchEvents = true;
+	DefaultMouseCursor = EMouseCursor::Crosshairs;
+}
---
+++ Source/MEDELT/MEDELTPlayerController.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MEDELTPlayerController.generated.h"
+
+/** PlayerController class used to enable cursor */
+UCLASS()
+class AMEDELTPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AMEDELTPlayerController();
+};
+
+
