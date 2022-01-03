251739dc864953b1c06ea22d72aeac1bb34af313
Onur <gardbrom@gmail.com>
2022-01-03 20:12:09 +0300

    database v1

--- Source/MEDELT/MEDELTPlayerController.cpp
+++ Source/MEDELT/MEDELTPlayerController.cpp
@@ -8,20 +8,9 @@ AMEDELTPlayerController::AMEDELTPlayerController()
 	bShowMouseCursor = true;
 	bEnableClickEvents = true;
 	bEnableTouchEvents = true;
-	DefaultMouseCursor = EMouseCursor::Crosshairs;
+	//FInputModeGameAndUI inputMode;
+ 	//inputMode.SetHideCursorDuringCapture(false);
+ 	//SetInputMode(inputMode);	
+	//DefaultMouseCursor = EMouseCursor::Crosshairs;
 }
 
-/*void AMEDELTPlayerController::SetupInputComponent() 
-{
-    // Always call this.
-    Super::SetupInputComponent();
-	if (InputComponent)
-    	InputComponent->BindAction("Shoot", IE_Pressed, this, &AMEDELTPlayerController::Shoot);
-}*/
-
-/*void AMEDELTPlayerController::Shoot()
-{
-	//GEngine->AddOnScreenDebugMessage(1, 1, FColor::Yellow, FString(TEXT("Fire")));
-	GetA
-	//MyTurret->Fire();
-}*/
